cfa4795e40b8b77313ba01a214c595286f2d5b87
Casey Marshall <github@cmars.tech>
2017-01-22 15:37:51 -0600

    Implement get/set script context commands.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+extern crate clap;
+extern crate futures;
+extern crate tokio_core;
+
+use std::env;
 use std::fmt;
 use std::fs::File;
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
+use std::net;
 use std::process::exit;
 use std::sync::mpsc;
 use std::{thread, time};
 
-extern crate clap;
 use clap::{Arg, ArgMatches, App, SubCommand};
+use futures::future::Future;
+use tokio_core::net::TcpStream;
+use tokio_core::reactor::Core;
 
 extern crate glop;
 use glop::grammar;
@@ -17,9 +25,11 @@ type AppResult<T> = Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
+    AddrParse(net::AddrParseError),
     CLI(clap::Error),
     IO(io::Error),
     Parse(glop::grammar::ParseError),
+    Env(env::VarError),
 }
 
 impl From<clap::Error> for Error {
@@ -34,19 +44,33 @@ impl From<io::Error> for Error {
     }
 }
 
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::CLI(ref err) => write!(f, "{}", err),
-            Error::IO(ref err) => write!(f, "{}", err),
-            Error::Parse(ref err) => write!(f, "{}", err),
-        }
+impl From<net::AddrParseError> for Error {
+    fn from(err: net::AddrParseError) -> Error {
+        Error::AddrParse(err)
+    }
+}
+
+impl From<env::VarError> for Error {
+    fn from(err: env::VarError) -> Error {
+        Error::Env(err)
     }
 }
 
 impl From<glop::grammar::ParseError> for Error {
     fn from(err: glop::grammar::ParseError) -> Error {
         Error::Parse(err)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::AddrParse(ref err) => write!(f, "{}", err),
+            Error::CLI(ref err) => write!(f, "{}", err),
+            Error::Env(ref err) => write!(f, "{}", err),
+            Error::IO(ref err) => write!(f, "{}", err),
+            Error::Parse(ref err) => write!(f, "{}", err),
+        }
     }
 }
 
@@ -75,7 +99,6 @@ fn main() {
         Some("agent") => cmd_agent(app_m.subcommand_matches("agent").unwrap()),
         Some("get") => cmd_get(app_m.subcommand_matches("get").unwrap()),
         Some("set") => cmd_set(app_m.subcommand_matches("set").unwrap()),
-        Some("send") => cmd_send(app_m.subcommand_matches("send").unwrap()),
         Some(subcmd) => {
             println!("unsupported command {}", subcmd);
             println!("{}", app_m.usage());
@@ -133,16 +156,62 @@ fn cmd_agent<'a>(app_m: &ArgMatches<'a>) -> AppResult<()> {
 }
 
 fn cmd_get<'a>(app_m: &ArgMatches<'a>) -> AppResult<()> {
-    println!("TODO: get");
+    let addr_str = env::var("ADDR").map_err(Error::Env)?;
+    let addr = addr_str.parse().map_err(Error::AddrParse)?;
+    let cmd = format!("get {}", app_m.value_of("KEY").unwrap());
+    let mut core = Core::new()?;
+    let handle = core.handle();
+    let socket = TcpStream::connect(&addr, &handle);
+    let request = socket.and_then(|mut sock| {
+        sock.write_all(cmd.as_bytes())?;
+        Ok(sock)
+    });
+    let response = request.and_then(|mut sock| {
+        let mut s = String::new();
+        sock.read_to_string(&mut s)?;
+        Ok(s)
+    });
+    let value = match core.run(response) {
+        Ok(data) => {
+            let mut values = data.split(" -> ");
+            match values.nth(1) {
+                Some(v) => v.to_string(),
+                None => {
+                    return Err(Error::IO(io::Error::new(io::ErrorKind::Other,
+                                                        "get: malformed response")));
+                }
+            }
+        }
+        Err(e) => {
+            return Err(Error::IO(e));
+        }
+    };
+    println!("{}", value);
     Ok(())
 }
 
 fn cmd_set<'a>(app_m: &ArgMatches<'a>) -> AppResult<()> {
-    println!("TODO: set");
-    Ok(())
-}
-
-fn cmd_send<'a>(app_m: &ArgMatches<'a>) -> AppResult<()> {
-    println!("TODO: send");
-    Ok(())
+    let addr_str = env::var("ADDR").map_err(Error::Env)?;
+    let addr = addr_str.parse().map_err(Error::AddrParse)?;
+    let cmd = format!("set {} {}",
+                      app_m.value_of("KEY").unwrap(),
+                      app_m.value_of("VALUE").unwrap());
+    let mut core = Core::new()?;
+    let handle = core.handle();
+    let socket = TcpStream::connect(&addr, &handle);
+    let request = socket.and_then(|mut sock| {
+        sock.write_all(cmd.as_bytes())?;
+        Ok(sock)
+    });
+    let response = request.and_then(|mut sock| {
+        let mut s = String::new();
+        sock.read_to_string(&mut s)?;
+        Ok(s)
+    });
+    match core.run(response) {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            return Err(Error::IO(e));
+        }
+    }
 }
