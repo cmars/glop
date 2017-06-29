b1c9a76eca5ef1ae6ae83460da7a3ed662db25e3
Casey Marshall <github@cmars.tech>
2017-06-29 22:58:28 +1000

    Add glop syntax example and started a lexer.

---
+++ lexer/lexer_test.go
@@ -0,0 +1,111 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/cmars/glop/token"
+)
+
+type expectedTokens struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
+
+func expect(t *testing.T, input string, expected []expectedTokens) {
+	l := New(input)
+	for i, tt := range expected {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},`
+	expect(t, input, []expectedTokens{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.EOF, ""},
+	})
+}
+
+func TestNextTokenKeywords(t *testing.T) {
+	input := "" +
+		`state start {
+	await {
+		message (topic "stuff", role "reader") msg {
+			log "got msg"
+		}
+		elapsed 15s {
+			log "timeout"
+		}
+	}
+	goto start
+} fault {
+	log "bad things"
+}`
+	expect(t, input, []expectedTokens{
+		{token.STATE, "state"},
+		{token.IDENT, "start"},
+		{token.LBRACE, "{"},
+		{token.AWAIT, "await"},
+		{token.LBRACE, "{"},
+		{token.MESSAGE, "message"},
+		{token.LPAREN, "("},
+		{token.IDENT, "topic"},
+		{token.STRING, "stuff"},
+		{token.COMMA, ","},
+		{token.IDENT, "role"},
+		{token.STRING, "reader"},
+		{token.RPAREN, ")"},
+		{token.IDENT, "msg"},
+		{token.LBRACE, "{"},
+		{token.LOG, "log"},
+		{token.STRING, "got msg"},
+		{token.RBRACE, "}"},
+		{token.ELAPSED, "elapsed"},
+		{token.DURATION, "15s"},
+		{token.LBRACE, "{"},
+		{token.LOG, "log"},
+		{token.STRING, "timeout"},
+		{token.RBRACE, "}"},
+		{token.RBRACE, "}"},
+		{token.GOTO, "goto"},
+		{token.IDENT, "start"},
+		{token.RBRACE, "}"},
+		{token.FAULT, "fault"},
+		{token.LBRACE, "{"},
+		{token.LOG, "log"},
+		{token.STRING, "bad things"},
+		{token.RBRACE, "}"},
+	})
+}
+
+func TestTokenizeSplit(t *testing.T) {
+	input := "split start { pinger | ponger } fault { goto start }"
+	expect(t, input, []expectedTokens{
+		{token.SPLIT, "split"},
+		{token.IDENT, "start"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "pinger"},
+		{token.PIPE, "|"},
+		{token.IDENT, "ponger"},
+		{token.RBRACE, "}"},
+		{token.FAULT, "fault"},
+		{token.LBRACE, "{"},
+		{token.GOTO, "goto"},
+		{token.IDENT, "start"},
+		{token.RBRACE, "}"},
+	})
+}
---
+++ token/token.go
@@ -0,0 +1,84 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENT    = "IDENT"
+	INT      = "INT"
+	STRING   = "STRING"
+	DURATION = "DURATION"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	MINUS  = "-"
+	EQUAL  = "=="
+	NEQUAL = "!="
+	GT     = ">"
+	LT     = "<"
+	GTE    = ">="
+	LTE    = "<="
+
+	// Delimiters
+	COMMA  = ","
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+	PIPE   = "|"
+
+	// Keywords
+	STATE     = "STATE"
+	FAULT     = "FAULT"
+	STATES    = "STATES"
+	SPLIT     = "SPLIT"
+	SPAWN     = "SPAWN"
+	GOTO      = "GOTO"
+	AWAIT     = "AWAIT"
+	MESSAGE   = "MESSAGE"
+	ELAPSED   = "ELAPSED"
+	ASSERT    = "ASSERT"
+	SEND      = "SEND"
+	WHEN      = "WHEN"
+	OTHERWISE = "OTHERWISE"
+	LOG       = "LOG"
+	SET       = "SET"
+	TRUE      = "TRUE"
+	FALSE     = "FALSE"
+)
+
+var keywords = map[string]TokenType{
+	"state":     STATE,
+	"fault":     FAULT,
+	"states":    STATES,
+	"split":     SPLIT,
+	"spawn":     SPAWN,
+	"goto":      GOTO,
+	"await":     AWAIT,
+	"message":   MESSAGE,
+	"elapsed":   ELAPSED,
+	"assert":    ASSERT,
+	"send":      SEND,
+	"when":      WHEN,
+	"otherwise": OTHERWISE,
+	"log":       LOG,
+	"set":       SET,
+	"true":      TRUE,
+	"false":     FALSE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
